9e4bf71045a82698d9019a31696cb8714b9aa620
P1keten <91056066+P1keten@users.noreply.github.com>
2021-10-17 00:59:21 +0200

    feature(support for multiple wash markers)

--- client/client.lua
+++ client/client.lua
@@ -1,22 +1,26 @@
 ESX = exports["es_extended"]:getSharedObject()
 
--- If you want to open the moneywash menu in another script. TriggerEvent("p1_moneywash:openWashMenu")
-RegisterNetEvent("p1_moneywash:openWashMenu")
-AddEventHandler("p1_moneywash:openWashMenu", function()
-    WashDirtyMoney()
-end)
-
 Citizen.CreateThread(function()
     while true do
         sleep = 500
-	local ped = PlayerPedId()
+        if Config.ShowBlip then
+            local blip = AddBlipForCoord(Config.EnterMoneyWash)
+        
+            SetBlipSprite(blip, Config.Blip.Icon)
+            SetBlipScale(blip, Config.Blip.Scale)
+            SetBlipColour(blip, Config.Blip.Color)
+            SetBlipAsShortRange(blip, true)
+        
+            BeginTextCommandSetBlipName("STRING")
+            AddTextComponentString(Config.Translation[Config.Language].blipName)
+            EndTextCommandSetBlipName(blip)
+        end
+	    local ped = PlayerPedId()
         local plyC = GetEntityCoords(ped, false)
         local enter = Config.EnterMoneyWash
         local leave = Config.InteriorLeave
-        local wash = Config.WashMoney
         local dist = #(plyC.xy - enter.xy)
         local dist2 = #(plyC.xy - leave.xy)
-        local dist3 = #(plyC.xy - wash.xy)
         if dist <= Config.ShowMarkerDist then
             sleep = 0
             DrawMarker(Config.MarkerType, enter.x, enter.y, enter.z-0.95, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 0, 0, 255, 200, 0, 0, 0, 0)
@@ -37,13 +41,16 @@ Citizen.CreateThread(function()
                 end
             end
         end
-        if dist3 <= Config.ShowMarkerDist then
-            sleep = 0
-            DrawMarker(Config.MarkerType, wash.x, wash.y, wash.z-0.95, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 0, 0, 255, 200, 0, 0, 0, 0)
-            if dist3 <= Config.ShowTextDist then
-                ESX.ShowHelpNotification(Config.Translation[Config.Language].washMoney)
-                if IsControlJustReleased(0, 51) then
-                    WashDirtyMoney()
+        for k,v in pairs(Config.WashMoney) do
+            local dist3 = #(plyC - v.coords)
+            if dist3 <= Config.ShowMarkerDist then
+                sleep = 0
+                DrawMarker(Config.MarkerType, v.coords.x, v.coords.y, v.coords.z-0.95, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 0, 0, 255, 200, 0, 0, 0, 0)
+                if dist3 <= Config.ShowTextDist then
+                    ESX.ShowHelpNotification(Config.Translation[Config.Language].washMoney)
+                    if IsControlJustReleased(0, 51) then
+                        TriggerEvent("p1_moneywash:openWashMenu")
+                    end
                 end
             end
         end
@@ -51,7 +58,9 @@ Citizen.CreateThread(function()
     end
 end)
 
-WashDirtyMoney = function()
+-- If you want to open the moneywash menu in another script; TriggerEvent("p1_moneywash:openWashMenu")
+RegisterNetEvent("p1_moneywash:openWashMenu")
+AddEventHandler("p1_moneywash:openWashMenu", function()
 
     ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'wash_amount', {
         title = Config.Translation[Config.Language].washTitle
@@ -68,27 +77,12 @@ WashDirtyMoney = function()
     end, function(data, menu)
         menu.close()
     end)
-end
+end)
 
 Teleport = function(x,y,z)
+    local ped = PlayerPedId()
     DoScreenFadeOut(100)
     Wait(600)
     SetEntityCoords(ped, x,y,z)
     DoScreenFadeIn(100)
 end
-
-Citizen.CreateThread(function()
-
-    if Config.ShowBlip then
-		local blip = AddBlipForCoord(Config.EnterMoneyWash)
-
-		SetBlipSprite(blip, Config.Blip.Icon)
-		SetBlipScale(blip, Config.Blip.Scale)
-		SetBlipColour(blip, Config.Blip.Color)
-		SetBlipAsShortRange(blip, true)
-
-		BeginTextCommandSetBlipName("STRING")
-		AddTextComponentString(Config.Translation[Config.Language].blipName)
-		EndTextCommandSetBlipName(blip)
-    end
-end)
