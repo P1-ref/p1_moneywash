54c2d9c0888692a699fa8c5366aabe749d3e4d32
P1keten <91056066+P1keten@users.noreply.github.com>
2021-10-16 10:41:33 +0200

    optimization(language support)

--- server/server.lua
+++ server/server.lua
@@ -10,31 +10,15 @@ AddEventHandler("p1_moneywash:washMoney", function(amount)
     if amount > 0 and xPlayer.getAccount("black_money").money >= amount then
         xPlayer.removeAccountMoney("black_money", amount)
         if Config.EnableTimer then
-            if Config.Language == "en" then
-                TriggerClientEvent("esx:showNotification", xPlayer.source, Config.LangEN["waitTime"]:format(math.floor(Config.TimeToWash)))
-                Citizen.Wait(waitTime)
-                TriggerClientEvent("esx:showNotification", xPlayer.source, Config.LangEN["youWashed"]:format(math.floor(amount)))
-                xPlayer.addMoney(amount)
-            else
-                TriggerClientEvent("esx:showNotification", xPlayer.source, Config.LangSV["waitTime"]:format(math.floor(Config.TimeToWash)))
-                Citizen.Wait(waitTime)
-                TriggerClientEvent("esx:showNotification", xPlayer.source, Config.LangSV["youWashed"]:format(math.floor(amount)))
-                xPlayer.addMoney(amount)
-            end
+            TriggerClientEvent("esx:showNotification", xPlayer.source, Config.Translation[Config.Language].waitTime:format(math.floor(Config.TimeToWash)))
+            Citizen.Wait(waitTime)
+            TriggerClientEvent("esx:showNotification", xPlayer.source, Config.Translation[Config.Language].youWashed:format(math.floor(amount)))
+            xPlayer.addMoney(amount)
         else
-            if Config.Language == "en" then
-                TriggerClientEvent("esx:showNotification", xPlayer.source, Config.LangEN["youWashed"]:format(math.floor(amount)))
-                xPlayer.addMoney(amount)
-            else
-                TriggerClientEvent("esx:showNotification", xPlayer.source, Config.LangSV["youWashed"]:format(math.floor(amount)))
-                xPlayer.addMoney(amount)
-            end
+            TriggerClientEvent("esx:showNotification", xPlayer.source, Config.Translation[Config.Language].youWashed:format(math.floor(amount)))
+            xPlayer.addMoney(amount)
         end
     else
-        if Config.Language == "en" then
-            TriggerClientEvent("esx:showNotification", xPlayer.source, Config.LangEN["notEnough"])
-        else
-            TriggerClientEvent("esx:showNotification", xPlayer.source, Config.LangSV["notEnough"])
-        end
+        TriggerClientEvent("esx:showNotification", xPlayer.source, Config.Translation[Config.Language].notEnough)
     end
 end)
